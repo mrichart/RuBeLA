ee557927dc26f8c054b72e7a2acb091998aa2e82
mrichart <mrichart@fing.edu.uy>
2013-09-18 15:43:56 -0300

    Add some utils

--- README.md
+++ README.md
@@ -5,14 +5,14 @@ This project consist of two different things:
 - A mechanism for link adaptation in wireless networks based on rules using the tau-fst library.
 - The NS3 Simulator with ns-3-dce module and some modifications to simulate the adaptation mechanism.
 
-Steps to configure the project after you clone it:
+Steps to configure and build the project after you clone it:
 
 - Download all the submodules:
   - git submodule init
   - git submodule update
 
 - Download bake: 
-  hg clone http://code.nsnam.org/bake
+  hg clone http://code.nsnam.org/bake (you need Mercurial installed)
 
 - Set variable for bake:
   export BAKE_HOME=`pwd`/bake
@@ -25,10 +25,22 @@ Steps to configure the project after you clone it:
   
 - Download dependencies:
   bake.py download
+  (If libc-debug is not found you need to install it. libc6-dbg in Ubuntu)
 
 - Build and install
   bake.py build
+  
+- Build lua static:
+  cd lua-static/luasocket-2.0.2/src
+  make static
+  cd ../../lua-static/lua-5.1.5/src
+  make ns3
+  cp lua ../../../utils
+  cd ../../..
 
 Running an example:
+
+- Copy some files into ns3:
+  sh config-dce-rate-loss.sh
 
   
---
+++ utils/config-dce-rate-loss.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+cant_ap=1
+
+aux_ap=$(($cant_ap-1))
+
+for i in `seq 0 $aux_ap`; do
+
+  mkdir ../ns-3-dce-git/"files-$i"
+
+	mkdir ../ns-3-dce-git/"files-$i/lupa"
+	
+	mkdir ../ns-3-dce-git/"files-$i/rnr"
+	
+	mkdir ../ns-3-dce-git/"files-$i/fsm"
+	
+	cp ./lua ../ns-3-dce-git/files-$i/
+	
+	cp -r ../LuPA/* ../ns-3-dce-git/files-$i/lupa/
+	
+	cp -r ../Rnr/* ../ns-3-dce-git/files-$i/rnr/
+	
+	cp -r ./fsm/* ../ns-3-dce-git/files-$i/fsm/
+	
+	echo "my_name='ap$i'
+
+my_name_pdp='AP$i/lupa/pdp'
+
+my_name_pdp='AP0/lupa/pdp'
+my_name_pep='NS3-PEP'
+my_name_rmoon='NS3-RMOON'
+
+my_host='127.0.0.1'
+
+my_localstate_port=9590
+
+upstream = '127.0.0.1'
+
+enable_rmoon = false
+enable_pep = false
+
+pdp_window_size = 10
+trap_generation_interval = 1
+
+local function randomize ()
+	local res = 0;                                 
+   local a, b, c, d = string.match(my_host, '(%d+)%.(%d+)%.(%d+)%.(%d+)');
+   res = math.pow(255,3)*a + math.pow(255,2)*b + 255*c + d;
+	math.randomseed(res);
+end;
+
+randomize()
+
+environment = 'environment'
+
+"  >> ../ns-3-dce-git/files-$i/lupa/"config_lamim_NS3.txt"
+
+###############################################################################
+
+echo "my_host='127.0.0.1'
+
+upstream[1] = '127.0.0.1'
+
+"  >> ../ns-3-dce-git/files-$i/rnr/"config_lamim_NS3.txt"
+
+done
